ea0b5fa730ff8a953669ad464cc012db3bca1105
Hyein Seo <gpdls0411@naver.com>
2026-04-11 13:34:28 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print(int N) {
+	if (N % 3 == 0 && N % 5 == 0) cout << "FizzBuzz" << "\n";
+	else if (N % 3 == 0 ) cout << "Fizz" << "\n";
+	else if ( N % 5 == 0) cout << "Buzz" << "\n";
+	else cout << N << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string str1, str2, str3;
+	cin >> str1;
+	cin >> str2;
+	cin >> str3;
+
+	if (str3 != "Fizz" && str3 != "Buzz" && str3 != "FizzBuzz") print(stoi(str3) + 1);
+	else if (str2 != "Fizz" && str2 != "Buzz" && str2 != "FizzBuzz") print(stoi(str2) + 2);
+	else print(stoi(str1) + 3);
+
+	return 0;
+}
